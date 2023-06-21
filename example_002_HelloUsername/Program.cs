6d9f3bc9d6dad584db60b55c3018cc2e0e32a714
yourenoticed <yourenoticed@gmail.com>
2023-06-21 23:53:06 +0700

    created and finished program for adding two numbers

--- example_002_HelloUsername/Program.cs
+++ example_002_HelloUsername/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Gets user's username and prints out "Hello, $username"
-Console.WriteLine("Input your username: ");
+Console.Write("Input your username: ");
 string username = Console.ReadLine();
 Console.Write("Hello, ");
 Console.Write(username);
---
+++ example_003_AddingNumbers/Program.cs
@@ -0,0 +1,4 @@
+﻿// Adding two numbers
+int number1 = 3;
+int number2 = 5;
+Console.WriteLine(number1 + number2);
