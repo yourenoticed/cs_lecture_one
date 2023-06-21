f152838218e82e319235edf8d428d14f59ae2aad
yourenoticed <yourenoticed@gmail.com>
2023-06-21 23:28:01 +0700

    added warning for inputting username

--- example_002_HelloUsername/Program.cs
+++ example_002_HelloUsername/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Gets user's username and prints out "Hello, $username"
+Console.WriteLine("Input your username: ");
 string username = Console.ReadLine();
 Console.Write("Hello, ");
 Console.Write(username);
