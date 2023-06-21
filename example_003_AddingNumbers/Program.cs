b5c4f1c870c79e72d59644943726d2e3cc597d5d
yourenoticed <yourenoticed@gmail.com>
2023-06-22 00:16:45 +0700

    added random to AddingNumbers and formatted it a bit

--- example_003_AddingNumbers/Program.cs
+++ example_003_AddingNumbers/Program.cs
@@ -1,4 +1,9 @@
 ﻿// Adding two numbers
-int number1 = 3;
-int number2 = 5;
+int number1 = new Random().Next(1, 11);
+int number2 = new Random().Next(1, 11);
+Console.Write("Number one: ");
+Console.WriteLine(number1);
+Console.Write("Number two: ");
+Console.WriteLine(number2);
+Console.Write("The sum of the numbers: ");
 Console.WriteLine(number1 + number2);
