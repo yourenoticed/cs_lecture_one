086c523cbe704e2215a140f621ffdafacc8d9e07
yourenoticed <yourenoticed@gmail.com>
2023-06-22 00:19:46 +0700

    added random to dividing numbers and formatted it

--- example_004_DividingNumbers/Program.cs
+++ example_004_DividingNumbers/Program.cs
@@ -1,4 +1,9 @@
 ﻿// Dividing two numbers
-double number1 = 12;
-double number2 = 5;
+double number1 = new Random().Next(1, 11);
+double number2 = new Random().Next(1, 11);
+Console.Write("Number one: ");
+Console.WriteLine(number1);
+Console.Write("Number two: ");
+Console.WriteLine(number2);
+Console.Write("The division of the numbers: ");
 Console.WriteLine(number1 / number2);
