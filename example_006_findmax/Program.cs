0c60fb5688837f8f7a5265f7dbd052cf9e4d53ad
yourenoticed <yourenoticed@gmail.com>
2023-06-26 13:39:56 +0700

    finished find_max algo using methods and arrays

---
+++ example_006_findmax/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] numbers = {39, 25, 13, 42, 32, 13, 52, 14, 15};
+
+int Max(int[] digits)
+{
+    int maximum = digits[0];
+    for (int i = 1; i < digits.Length; i++)
+    {
+        if (maximum < digits[i])
+        {
+            maximum = digits[i];
+        }
+    }
+    return maximum;
+}
+Console.WriteLine(Max(numbers));
