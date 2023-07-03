6b1b24c9a2233de6470bc55a443a538f902be7a0
yourenoticed <yourenoticed@gmail.com>
2023-07-03 15:20:08 +0700

    finished lecture 3

---
+++ example_007_replace_chars/Program.cs
@@ -0,0 +1,19 @@
+﻿string ReplaceChars (string text, char oldVal, char newVal)
+{
+    string result = string.Empty;
+    foreach (char c in text)
+    {
+        if (c != oldVal)
+        {
+            result = $"{result}{c}";
+        }
+        else
+        {
+            result = $"{result}{newVal}";
+        }
+    }
+    return result;
+} 
+
+string text = "I fucked your mommas mouth you sucker";
+System.Console.WriteLine(ReplaceChars(text: text, 'u', '*'));
---
+++ example_008_sort_array/Program.cs
@@ -0,0 +1,49 @@
+﻿void SortArray (int[] array)
+{
+    int sorted = 0;
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        for (int j = 0; j < array.Length - sorted - 1; j++)
+        {
+            
+            if (array[j] > array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+                
+            }
+        }
+        sorted++;
+    }
+    
+}
+
+int[] CreateAndFillArrayWithRandomNums (int size, int start, int end)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(start, end);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    foreach (int i in array)
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+}
+
+void SortCreatedArrayAndShowTheResults (int size, int start, int end)
+{
+    int[] ints = CreateAndFillArrayWithRandomNums(size, start, end);
+    PrintArray(ints);
+    SortArray(ints);
+    PrintArray(ints);
+}
+
+SortCreatedArrayAndShowTheResults(10, 1, 98);
