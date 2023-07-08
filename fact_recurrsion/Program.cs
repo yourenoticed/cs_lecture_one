6f7fb0e84c18e808a5f2c3394c90f1ee11c6f46c
yourenoticed <yourenoticed@gmail.com>
2023-07-08 17:27:59 +0700

    finished reccursion and n-dimensional arrays

---
+++ fact_recurrsion/Program.cs
@@ -0,0 +1,28 @@
+﻿double Factorial(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
+
+int Fibonacci(int n)
+{
+    
+    if (n == 1)
+    {
+        return 1;
+    }
+    if (n == 2)
+    {
+        return 1;
+    }
+    else return Fibonacci(n-1) + Fibonacci(n - 2);
+}
+
+System.Console.WriteLine(Fibonacci(6));
+
---
+++ matrix/Program.cs
@@ -0,0 +1,28 @@
+﻿int[,] FillMatrix(int rows, int columns, int start, int end) // also prints it out
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = new Random().Next(start, end + 1);
+        }
+    }
+    PrintMatrix(matrix);
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] ints = FillMatrix(3, 5, 1, 10);
+
